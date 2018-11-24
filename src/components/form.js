6a8bf9ce2454b6d6fa7d9b93841a4ff8ff474032
VladADragos <vlad6620@yahoo.com>
2018-11-24 15:01:49 +0100

    fixed netlify form

--- src/components/form.js
+++ src/components/form.js
@@ -1,44 +1,35 @@
 import React, { Component } from "react";
-import * as firebase from "firebase";
+
 export class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
     // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyDO8SnYoKpiwIkgnmAEd9qa_J6U5_slngk",
-      authDomain: "sendemail-d46a1.firebaseapp.com",
-      databaseURL: "https://sendemail-d46a1.firebaseio.com",
-      projectId: "sendemail-d46a1",
-      storageBucket: "sendemail-d46a1.appspot.com",
-      messagingSenderId: "396400764517"
-    };
-    firebase.initializeApp(config);
-
-    let messagesRef = firebase.database().ref("messages");
-
-    const handleSubmit = e => {
-      e.preventDefault();
-      let name = e.target.name.value;
-      let email = e.target.email.value;
-      let message = e.target.message.value;
-      console.log(name, email, message);
-      saveMessage(name, email, message);
-    };
-
-    const saveMessage = (name, email, message) => {
-      let newMessageRef = messagesRef.push();
-      newMessageRef.set({
-        name: name,
-        email: email,
-        message: message
-      });
-    };
 
     return (
       <form
         className="contact-form"
-        name="contact"
         data-netlify="true"
-        method="POST"
+        onSubmit={this.handleSubmit}
+        name="contact-form"
       >
         <div className="contact-form__input--row">
           <input
