3ff9330d7a7d0358eeaf5f49c4ec55ca6dd561ce
VladADragos <vlad6620@yahoo.com>
2018-11-24 15:37:00 +0100

    fixed netlify form

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 import { Info } from "./info";
 import { Form } from "./form";
+import { ContactForm } from "./ContactForm";
 
 export class Footer extends Component {
   render() {
     return (
       <section className="footer" id="footer">
         <div className="footer__container">
-          <Form />
+          {/*<Form /> */}
           <Info />
+          <ContactForm />
         </div>
       </section>
     );
