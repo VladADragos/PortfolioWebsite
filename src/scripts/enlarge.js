8e66e8fd79bdaaae2cdeadd6e85a35ee3d679215
VladADragos <vlad6620@yahoo.com>
2018-12-02 15:12:22 +0100

    mobile responsive

--- src/scripts/enlarge.js
+++ src/scripts/enlarge.js
@@ -1,14 +1,12 @@
-const enlarge =(e)=>{
-    
-    let parent = e.currentTarget.parentNode;
-    let target = e.currentTarget;    
-    for(let i=0;i<3;i++){
-      if(parent.childNodes[i].classList[1] =="enlarge"){
-        parent.childNodes[i].classList.remove("enlarge");
-        target.classList.add("enlarge");
-      }
+const enlarge = e => {
+  let parent = e.currentTarget.parentNode;
+  let target = e.currentTarget;
+  console.log(target);
+  for (let i = 0; i < 3; i++) {
+    if (parent.childNodes[i].classList[1] == "enlarge") {
+      parent.childNodes[i].classList.remove("enlarge");
+      target.classList.add("enlarge");
     }
-    
- 
   }
-  export default enlarge;
+};
+export default enlarge;
