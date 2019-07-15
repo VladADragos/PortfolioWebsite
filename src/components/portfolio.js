9ba698d24548bc506f26598715c46f7dab43a764
VladADragos <vlad6620@yahoo.com>
2019-07-15 18:46:41 +0200

    updated portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,50 +4,104 @@ import { PortfolioItem } from "./portfolioItem";
 export class Portfolio extends Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    // tags: [
+    //   ["React", "SASS", "JavaScript"],
+    //   ["React", "SASS", "Axios", "API"],
+    //   ["PHP", "SASS", "JavaScript", "MySQL"]
+    // ],
+    // textBody: [
+    //   [
+    //     "A simple calculator app build with React and styled with SASS.",
+    //     "The app makes use of the eval function that turns strings into javascript statements.",
+    //     "Validation is implemented to avoid errors and stop malicious code from being run"
+    //   ],
+    //   ["WORK IN PROGRESS", "Schedule/Productivity app"],
+    //   [
+    //     "Semy finished(working version 1.0)",
+    //     "Social media app for cats",
+    //     "User creation,edit & delete. Login & logout",
+    //     "Password encryption",
+    //     "Validation on all the things"
+    //   ]
+    // ]
+    // };
+
     this.state = {
-      calculator: {
-        tags: [
-          ["React", "SASS", "JavaScript"],
-          ["React", "SASS", "Express.js", "mongodb"],
-          ["PHP", "SASS", "JavaScript", "MySQL"]
-        ],
-        textBody: [
-          [
+      portfolio: [
+        {
+          title: "React-Calculator",
+          url: "/React-Calculator",
+          isLive: true,
+          tags: ["React", "JavaScript"],
+          enlargen: "enlarge",
+          text: [
             "A simple calculator app build with React and styled with SASS.",
             "The app makes use of the eval function that turns strings into javascript statements.",
             "Validation is implemented to avoid errors and stop malicious code from being run"
-          ],
-          ["WORK IN PROGRESS", "Schedule/Productivity app"],
-          ["WORK IN PROGRESS", "Social Media App for cats"]
-        ]
-      }
+          ]
+        },
+        {
+          title: "Catbook",
+          url: "/http://www.catbook.se/",
+          isLive: true,
+          tags: ["PHP", "JavaScript", "CSS", "SQL"],
+          enlargen: "",
+          text: [
+            "WORK IN PROGRESS",
+            "Semy finished(working version 1.0)",
+            "Social media app for cats",
+            "User creation,edit & delete. Login & logout",
+            "Password encryption",
+            "Validation on all the things"
+          ]
+        },
+        {
+          title: "Openstocks",
+          url: "/http://www.openstocks.se/",
+          isLive: false,
+          tags: ["React", "Node", "Express", "TS", "Charts"],
+          enlargen: "",
+          text: [
+            "WORK IN PROGRESS",
+            "Partially working",
+            "Fetches api data through an express server to avoid cors errors and hide api keys",
+            "Data displayed in chart and table form"
+          ]
+        }
+      ]
     };
   }
+
+  enlarge = e => {
+    let title = e.currentTarget.id;
+    this.state.portfolio.map(portfolioItem => {
+      if (
+        portfolioItem.enlargen === "enlarge" &&
+        portfolioItem.title !== title
+      ) {
+        portfolioItem.enlargen = "";
+        console.log("made smaller");
+      } else if (portfolioItem.title === title) {
+        portfolioItem.enlargen = "enlarge";
+        console.log("made biggwe");
+      }
+    });
+    this.setState({ portfolio: this.state.portfolio });
+  };
   render() {
     return (
       <div className="portfolio">
         <div className="portfolio__grid">
-          <PortfolioItem
-            title="React-Calculator"
-            enlargen="enlarge"
-            url="/React-Calculator"
-            tags={this.state.calculator.tags[0]}
-            text={this.state.calculator.textBody[0]}
-          />
-          <PortfolioItem
-            title="Routiner"
-            enlargen=""
-            url="/"
-            tags={this.state.calculator.tags[1]}
-            text={this.state.calculator.textBody[1]}
-          />
-          <PortfolioItem
-            title="Catbook"
-            enlargen=""
-            url="http://www.catbook.se/"
-            tags={this.state.calculator.tags[2]}
-            text={this.state.calculator.textBody[2]}
-          />
+          {this.state.portfolio.map(portfolioItem => {
+            return (
+              <PortfolioItem
+                key={portfolioItem.url}
+                portfolioItemData={portfolioItem}
+                enlarge={this.enlarge}
+              />
+            );
+          })}
         </div>
       </div>
     );
--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -1,45 +1,62 @@
 import React, { Component } from "react";
-import enlarge from "./../scripts/enlarge";
 import { NavLink } from "react-router-dom";
 export class PortfolioItem extends Component {
   render() {
+    let {
+      title,
+      url,
+      isLive,
+      tags,
+      enlargen,
+      text
+    } = this.props.portfolioItemData;
     return (
       <div
-        className={`portfolio-item ${this.props.enlargen}`}
-        onClick={enlarge}
+        className={`portfolio-item ${enlargen}`}
+        onClick={this.props.enlarge}
+        id={title}
       >
         <img
           className="portfolio-item__image"
-          src={require(`../images/${this.props.title}.png`)}
+          src={require(`../images/${title}.png`)}
           alt=""
         />
         <div className="portfolio-item__info-container">
           <div className="portfolio-item___text-container">
-            <h2 className="portfolio-item__title">{this.props.title}</h2>
-            <p className="portfolio-item__paragraph"> {this.props.text[0]}</p>
-            <p className="portfolio-item__paragraph"> {this.props.text[1]}</p>
-            <p className="portfolio-item__paragraph"> {this.props.text[2]}</p>
+            <h2 className="portfolio-item__title">{title}</h2>
+            {text.map(p => {
+              return (
+                <p key={p} className="portfolio-item__paragraph">
+                  {" "}
+                  {p}
+                </p>
+              );
+            })}
           </div>
           <div className="portfolio-item__buttons-container">
             <div className="portfolio-item__links-container">
+              {isLive && (
+                <a
+                  href={url}
+                  className="portfolio-item__button"
+                  target="_blank"
+                >
+                  View Demo
+                </a>
+              )}
               <a
-                href={this.props.url}
-                className="portfolio-item__button"
                 target="_blank"
-              >
-                View Demo
-              </a>
-              <a
-                target="_blank"
-                href={`https://github.com/VladADragos/${this.props.title}`}
+                href={`https://github.com/VladADragos/${title}`}
                 className="portfolio-item__button portfolio-item__button--github"
               >
                 Github
               </a>
             </div>
             <div className="portfolio-item__tags-container">
-              {this.props.tags.map(tag => (
-                <p className="portfolio-item__tag">{tag}</p>
+              {tags.map(tag => (
+                <p key={tag} className="portfolio-item__tag">
+                  {tag}
+                </p>
               ))}
             </div>
           </div>
