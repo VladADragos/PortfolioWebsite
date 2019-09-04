695fc294fc2fbb04c327e9d1dc6dcc2b8bd36ef4
VladADragos <vlad6620@yahoo.com>
2019-09-04 17:33:13 +0200

    Signed-off-by: VladADragos <vlad6620@yahoo.com>

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Main } from './main';
 import { About } from './about';
 import { PortfolioHeader } from '../portfolioHeader';
-import { Portfolio } from '../Portfolio';
+// import { Portfolio } from '../Portfolio';
 import { Footer } from '../layout/footer';
 import { scrollToPortfolio } from '../../scripts/smoothScroll';
 export class Home extends Component {
@@ -13,7 +13,7 @@ export class Home extends Component {
         <div className='ocean'>
           <About />
           <PortfolioHeader />
-          <Portfolio />
+          {/* <Portfolio /> */}
           <Footer />
         </div>
       </div>
