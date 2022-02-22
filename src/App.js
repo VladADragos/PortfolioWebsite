7ee5d787fb21683fe50359a21061ac4a2ed0a800
VladADragos <vlad6620@yahoo.com>
2022-02-22 14:58:06 +0100

    #1: fixed things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './css/main.css';
-import { Home } from './components/pages/Home';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./css/main.css";
+import { Home } from "./components/pages/Home";
 
-import { Calculator } from './components/portfolioItems/Calculator';
+import { Calculator } from "./components/portfolioItems/Calculator";
 
-function temp(){
-	
-	return <React.Fragment></React.Fragment>;
+function temp() {
+  return <React.Fragment></React.Fragment>;
+}
+function ComponentThatDoesStuff() {
+  return <div></div>;
 }
 
-function App(){
-  return ( <BrowserRouter>
-    <Switch>
-      <Route path='/' component={Home} exact />
-<Route path='/zohoverify' component={temp} />
-      <Route path='/React-Calculator' component={Calculator} />
-    </Switch>
-  </BrowserRouter>)
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/zohoverify" component={temp} />
+        <Route path="/React-Calculator" component={Calculator} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
 
 export default App;
