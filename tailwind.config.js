16a20080fe1fb8f7712e07d01fa57a61d0005eaa
VladADragos <vlad6620@yahoo.com>
2022-03-23 23:20:31 +0100

    added untracked files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,38 @@
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      screens: {
+        "desktop-sm": "800px",
+        "desktop-md": "1500px",
+        "desktop-lg": "2000px",
+      },
+      animation: {
+        spin: "spin 4s linear infinite",
+      },
       colors: {
         brand: {
-          blue:{
+          blue: {
+            light: "#404cbf",
+            medium: "#0A125C",
             dark: "#0B124E",
-            medium: "#0A125C"
           },
           orange: {
-            medium : "#F4890B"
-          }
-        }
+            light: "#f09d3e",
+            medium: "#F4890B",
+          },
+        },
       },
       fontFamily: {
         poppins: "'Poppins', sans-serif",
         pacifico: "'Pacifico', cursive",
         satisfy: "'Satisfy', cursive",
-      }
-    }
+      },
+    },
   },
-    
-
 
   plugins: [],
-}
+};
